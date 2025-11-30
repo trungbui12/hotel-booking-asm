9a33d051fc1bd7cec31aa32b5f938eed3a86ae1d
trungbui12 <yuuichirou67@gmail.com>
2025-11-30 09:20:22 +0700

    ok

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -116,8 +116,8 @@ export const roomCommonData = [
 // User Dummy Data
 export const userDummyData = {
     "_id": "user_2unqyL4diJFP1E3pIBnasc7w8hP",
-    "username": "Great Stack",
-    "email": "user.greatstack@gmail.com",
+    "username": "Trung Bùi",
+    "email": "trung05.vanxuan.thienkhoi@gmail.com",
     "image": "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
     "role": "hotelOwner",
     "createdAt": "2025-03-25T09:29:16.367Z",
